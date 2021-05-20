ab8b5fc0a5b46a899c47f310ab3a1cee073816ec
MTs Technonatura <80556134+mts-technonatura@users.noreply.github.com>
2021-05-20 09:14:47 +0700

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -174,7 +174,15 @@ const CenterHeader = ({ theme, townName, townTagLine }) => {
         </Link>
         <HeaderRightZone>
           <Link href="#" className="it-search-wrapper text-white text-decoration-none">
-            <HeaderSearch label="Search Engine" iconName="it-search" />
+              <div className="it-search-wrapper">
+                <span class="d-none d-md-block">Search Engine</span>
+                <a class="search-link rounded-icon" ariaLabel="Search Engine">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="icon m-0">
+                    <path d="M21.9 21.1L16 15.3a8.3 8.3 0 002-5.3 8 8 0 10-8 8 8.3 8.3 0 005.3-2l5.8 5.9zM10 17a7 7 0 117-7 7 7 0 01-7 7z"/>
+                    <path fill="none" d="M0 0h24v24H0z"/>
+                  </svg>
+                </a>
+              </div>
           </Link>
         </HeaderRightZone>
       </HeaderContent>
